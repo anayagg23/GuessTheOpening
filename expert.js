b26ff777092cf379e5980ab540d20962cdcbec68
Mathemagician <76497184+Mathemagician123@users.noreply.github.com>
2022-03-28 13:05:09 -0700

    Touchscreen Issue Fix

--- expert.js
+++ expert.js
@@ -214,4 +214,6 @@ function preventScroll(e){
 
 updateStatus()
 
-board.addEventListener('wheel', preventScroll, {passive: false});
+jQuery(board).on('scroll touchmove touchend touchstart contextmenu', function(e){
+  e.preventDefault();
+  });
